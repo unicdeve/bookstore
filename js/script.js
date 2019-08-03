51735a4424f1e2c442bae513f417d2ccee283056
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-08-03 12:18:52 +0100

    gallery with lightbox

--- js/script.js
+++ js/script.js
@@ -1,34 +1,16 @@
+$(document).ready(function () {
+  lightbox.option({
+    'wrapAround': true
+  });
 
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
 
+    if (position >= 350) {
+      $('.gallery').addClass('change');
+    } else {
+      $('.gallery').removeClass('change');
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  });
+});
